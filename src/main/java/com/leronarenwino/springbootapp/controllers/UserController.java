ac1519aba6540be023a608f2d5ee5923753f2fc9
Francisco Dueñas <frsduenasca@unal.edu.co>
2024-01-11 23:42:38 -0500

    :sparkles: [UPDATE] Detail view

--- src/main/java/com/leronarenwino/springbootapp/controllers/UserController.java
+++ src/main/java/com/leronarenwino/springbootapp/controllers/UserController.java
@@ -1,13 +1,15 @@
 package com.leronarenwino.springbootapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping("/details")
     public String details(){
 
-        return "details"
+        return "details";
 
     }
 
