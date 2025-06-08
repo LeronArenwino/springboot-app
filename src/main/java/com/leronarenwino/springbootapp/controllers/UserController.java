2cfe89f49ada3cc3fdcde84093ca9aeba2c4701d
Francisco Dueñas <Sebasduca@gmail.com>
2025-06-07 19:53:37 -0500

    :sparkles: [UPDATE] Enhance details view with Thymeleaf integration

--- src/main/java/com/leronarenwino/springbootapp/controllers/UserController.java
+++ src/main/java/com/leronarenwino/springbootapp/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.leronarenwino.springbootapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model) {
+        model.addAttribute("title", "User Details");
+        model.addAttribute("name", "Leron");
+        model.addAttribute("lastname", "Arenwino");
 
         return "details";
 
